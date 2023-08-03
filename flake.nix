35a9070f467036d3654ea7b4a580c0086a4a3a35
Able Arthy <ablearthy@gmail.com>
2023-08-03 09:13:10 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        nodejs = pkgs.nodejs_20;
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+            nodePackages.svelte-language-server
+            nodePackages.eslint
+          ];
+        };
+      });
+}
