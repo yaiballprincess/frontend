86e1f0f5c6b703e2a16f4df4b45c23d3f64f0102
Able Arthy <ablearthy@gmail.com>
2023-08-08 16:04:28 +0300

    Specialize fetchProtected

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -4,14 +4,29 @@ import { host } from '$lib/config';
 import jwtDecode from 'jwt-decode';
 import type { JwtPayload } from 'jwt-decode';
 import { goto } from '$app/navigation';
+import { redirect } from '@sveltejs/kit';
 
 export const accessToken = store('');
 
-export async function fetchProtected(input: RequestInfo | URL, init?: RequestInit) {
+export async function fetchProtectedOrGoto(input: RequestInfo | URL, init?: RequestInit) {
+	return genericFetchProtected(input, init, () => goto('/auth'));
+}
+
+export async function fetchProtectedOrRedirect(input: RequestInfo | URL, init?: RequestInit) {
+	return genericFetchProtected(input, init, () => {
+		throw redirect(308, '/auth');
+	});
+}
+
+async function genericFetchProtected(
+	input: RequestInfo | URL,
+	init: RequestInit | undefined,
+	onFailure: () => void
+) {
 	if (!isValidAccessToken(accessToken.get())) {
 		const d = await tryFetchAndSetNewAccessToken();
 		if (d === undefined) {
-			goto('/auth');
+			onFailure();
 			return;
 		}
 	}
