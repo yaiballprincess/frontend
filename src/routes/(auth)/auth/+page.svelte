7ca31ae487804e946dfcc571d536d4ec7b9ca788
Able Arthy <ablearthy@gmail.com>
2023-08-26 17:18:32 +0300

    Reformat files

--- src/routes/(auth)/auth/+page.svelte
+++ src/routes/(auth)/auth/+page.svelte
@@ -1,59 +1,59 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
-    import { accessToken } from '$lib/fetch';
+	import { goto } from '$app/navigation';
+	import { accessToken } from '$lib/fetch';
 
-    let error;
+	let error;
 
-    async function handleSubmit(event: Event) {
-        const data = new FormData(event.target as HTMLFormElement);
+	async function handleSubmit(event: Event) {
+		const data = new FormData(event.target as HTMLFormElement);
 
-        const response = await fetch('/api/sessions', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: data.get('username'),
-                password: data.get('password')
-            })
-        });
-        if (!response.ok) {
-            error = await response.json();
-            return;
-        }
+		const response = await fetch('/api/sessions', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				username: data.get('username'),
+				password: data.get('password')
+			})
+		});
+		if (!response.ok) {
+			error = await response.json();
+			return;
+		}
 
-        const newAccessToken = await response.json();
-        accessToken.set(newAccessToken);
-    goto("/");
-    }
+		const newAccessToken = await response.json();
+		accessToken.set(newAccessToken);
+		goto('/');
+	}
 </script>
 
 <main class="container">
-    <form method="POST" on:submit|preventDefault={handleSubmit}>
-        <label for="username">
-            Username
-            <input type="text" id="username" name="username" required />
-        </label>
-        <label for="password">
-            Password
-            <input type="password" id="password" name="password" required />
-        </label>
-        <button type="submit">Sign in</button>
-    </form>
-    {#if error}
-        <p>{error.message}</p>
-        {#if 'details' in error}
-            <ul>
-                {#each error.details as e}
-                    <li>{e}</li>
-                {/each}
-            </ul>
-        {/if}
-    {/if}
+	<form method="POST" on:submit|preventDefault={handleSubmit}>
+		<label for="username">
+			Username
+			<input type="text" id="username" name="username" required />
+		</label>
+		<label for="password">
+			Password
+			<input type="password" id="password" name="password" required />
+		</label>
+		<button type="submit">Sign in</button>
+	</form>
+	{#if error}
+		<p>{error.message}</p>
+		{#if 'details' in error}
+			<ul>
+				{#each error.details as e}
+					<li>{e}</li>
+				{/each}
+			</ul>
+		{/if}
+	{/if}
 </main>
 
 <style>
-    main {
-        max-width: 576px;
-    }
+	main {
+		max-width: 576px;
+	}
 </style>
--- src/routes/(protected)/+page.ts
+++ src/routes/(protected)/+page.ts
@@ -4,7 +4,7 @@ import { processRules } from '$lib/rule';
 export const ssr = false;
 
 export const load = async () => {
-    const response = await fetchProtectedOrRedirect('/api/rules');
-    const data = await response?.json();
-    return { rules: processRules(data) };
+	const response = await fetchProtectedOrRedirect('/api/rules');
+	const data = await response?.json();
+	return { rules: processRules(data) };
 };
