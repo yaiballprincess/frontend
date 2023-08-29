45d7ef8666e7ebe5dbbf44cc92a45a8d96ba23de
Able Arthy <ablearthy@gmail.com>
2023-08-29 19:30:20 +0300

    Implement DateTimeInput component

---
+++ src/lib/components/DateTimeInput.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let value: Date | undefined;
+	const padNumber = (n) => n.toString().padStart(2, '0');
+
+	const formatToDateTimeLocal = (d) => {
+		console.log(d);
+		return d !== undefined
+			? `${d.getFullYear()}-${padNumber(d.getMonth() + 1)}-${padNumber(d.getDate())}T${padNumber(
+					d.getHours()
+			  )}:${padNumber(d.getMinutes())}`
+			: undefined;
+	};
+	const onChange = () => {
+		value = dateTimeLocalString ? new Date(dateTimeLocalString) : undefined;
+	};
+	let dateTimeLocalString = formatToDateTimeLocal(value);
+	$: dateTimeLocalString = formatToDateTimeLocal(value);
+</script>
+
+<input
+	type="datetime-local"
+	bind:value={dateTimeLocalString}
+	on:input={onChange}
+	{...$$restProps}
+/>
