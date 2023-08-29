ef69f6772e61ef9c7b702cfc71e0d8b282ffd1a7
Able Arthy <ablearthy@gmail.com>
2023-08-29 16:33:22 +0300

    Update main page

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -1,17 +1,59 @@
 <script lang="ts">
 	import Rule from '$lib/components/Rule.svelte';
+	import PollTemplate from '$lib/components/PollTemplate.svelte';
+	import { pollTemplates } from '$lib/poll-template';
+	import { senders } from '$lib/sender';
+	import { store } from '$lib/store';
+	import { onMount } from 'svelte';
 	export let data;
+
+	let newPollTemplate;
+	function resetNewPollTemplate() {
+		newPollTemplate = {
+			question: '',
+			options: '',
+			isMultiple: false,
+			isAnonymous: false
+		};
+	}
+	resetNewPollTemplate();
 </script>
 
 <main class="container">
-	<section class="kb-cards">
-		{#each Object.entries(data.rules) as [id, rule]}
-			<Rule {rule} />
-		{/each}
+	<section id="poll-templates">
+		<h1>Poll templates</h1>
+		<div class="kb-cards">
+			{#each Object.entries($pollTemplates) as [id, pollTemplate]}
+				<PollTemplate state="view" data={pollTemplate} />
+			{/each}
+			<PollTemplate state="edit" data={newPollTemplate} onCreation={resetNewPollTemplate} />
+		</div>
+	</section>
+
+	<section id="senders">
+		<h1>Senders</h1>
+		<div class="kb-cards">
+			{#each Object.entries($senders) as [id, sender]}
+				<div>{id}</div>
+			{/each}
+		</div>
+	</section>
+
+	<section id="rules">
+		<h1>Rules</h1>
+		<div class="kb-cards">
+			{#each Object.entries(data.rules) as [id, rule]}
+				<Rule {rule} />
+			{/each}
+		</div>
 	</section>
 </main>
 
 <style>
+	main.container {
+		max-width: 1280px;
+		margin: 0 auto;
+	}
 	.kb-cards {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
